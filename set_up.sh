0bcb8f81ce633765a63305b7f0f4cc725d889822
naveensarathi <naveensarathi@gmail.com>
2020-02-04 20:18:55 +0800

    Create set_up.sh

---
+++ set_up.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ "$#" -ne 2 ]
+then
+        echo "Usage: ./set_up.sh HOSTNAME IP_ADDRESS"
+
+else
+wh='\033[0;97m'
+br1='\033[0;32m'
+br2='\033[0;91m'
+NC='\033[0m'
+echo -e "\t\t\tHOSTNAME: $wh$1$NC \n\t\t\tIP:$wh$2$NC \n\t\tHit$br1 Any$NC key to continue or $br2^c$NC to cancle "
+read
+hostnamectl set-hostname $1
+sed "s/192.168.5.237/$2/g" /etc/netplan/50-cloud-init.yaml > /etc/netplan/50-cloud-init.tmp
+#cat /etc/netplan/50-cloud-init.tmp
+cp /etc/netplan/50-cloud-init.tmp /etc/netplan/50-cloud-init.yaml
+
+fi
